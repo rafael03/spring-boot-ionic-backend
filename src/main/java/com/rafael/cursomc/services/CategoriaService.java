a91c5455dad59a20ef16c4d1014e42a577b05765
Rafael Pereira <rafaelrps11@gmail.com>
2019-09-23 08:01:59 -0300

    Ajuste no Endpoint /categorias/{id} , e retorno do erro

--- src/main/java/com/rafael/cursomc/services/CategoriaService.java
+++ src/main/java/com/rafael/cursomc/services/CategoriaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafael.cursomc.domain.Categoria;
+import com.rafael.cursomc.exceptions.ObjectNotFoundException;
 import com.rafael.cursomc.repositories.CategoriaRepository;
 
 @Service
@@ -16,6 +17,7 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 }
